5c374166d448936a1874f792abc7ab6da4bc966f
Ethan Winn <ethan@echoditto.com>
2012-02-13 00:44:48 -0500

    Added support for Services' Node Resources' Index.
    Using Backbone collections, with a child that abstracts Service's REST parameters.

--- js/drupalbackbone.js
+++ js/drupalbackbone.js
@@ -9,40 +9,97 @@
 
 (function($) {
 
-  $(function() {
-    // Drupal.Backbone Constructor, currently a no-op
-    Drupal.Backbone = function() {};
+  Drupal.behaviors.backbone = {
+    attach: function() {
+      // Drupal.Backbone Constructor, currently a no-op
+      Drupal.Backbone = function() {};
 
-    // Extend the Model object with default Drupal Services settings and methods.
-    // These are defaults for interfacing with all Service module's providers.
-    Drupal.Backbone.Model = Backbone.Model.extend({
-      // Use the Drupal Services REST format for nodes, /endpoint/{{nid}}.json.
-      // We don't include the collection stuff here, since Drupal collections are
-      // indpendent of their objects.
-      url: function() {
-        // Modified from Backbone.js to ignore collection and add ".format" extension.
-        var base = this.urlRoot || urlError();
-        if (this.isNew()) { return base; }
-        // Add .json for format here.
-        return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id) + ".json";
+      // Base objects for Drupal Backbone implementation.
+
+      // Extend the Model object with default Drupal Services settings and methods.
+      // These are defaults for interfacing with all Service module's providers.
+      Drupal.Backbone.Model = Backbone.Model.extend({
+        // Use the Drupal Services REST format for nodes, /endpoint/{{nid}}.json.
+        // We don't include the collection stuff here, since Drupal collections are
+        // indpendent of their objects.
+        url: function() {
+          // Modified from Backbone.js to ignore collection and add ".format" extension.
+          var base = this.urlRoot || urlError();
+          if (this.isNew()) { return base; }
+          // Add .json for format here.
+          return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.id) + ".json";
+          }
+      });
+
+      // Currently just sets the endpoint for all collections.
+      Drupal.Backbone.Collection = Backbone.Collection.extend({
+        // Base endpoint, used to create full url for each collection.
+        restEndpoint: "/backbone/rest",
+
+        // Drupal collections are stateful, we store params in the collection.
+        params: {},
+
+        // Initialize function allows params to be passed at object creation.
+        initialize: function(params) {
+          if (params) {
+            this.setParams(params);
+          }
+        },
+
+        // Setter for individual params, called as setParam('name','value').
+        setParam: function(paramName, paramValue) {
+          this.params[paramName] = paramValue;
+        },
+        // Setter for multiple params, passed as object with key/value pairs.
+        setParams: function(params) {
+          if (typeof(params) === 'Object') {
+            _.extend(
+              this.params,
+              params
+            );
+          }
+        },
+        // Getter. Currently just returns param object property.
+        getParams: function() {
+          return this.params;
+        },
+
+        // Fetch method passes params as data in AJAX call.
+        fetch: function(options) {
+          // Allow options.data to override any params.
+          _.defaults(options.data, this.getParams());
+          // Call Super fetch function with options array including any collection params.
+          Backbone.Collection.prototype.fetch.call(this, options);
         }
-    });
+      });
 
-    // Node-specific settings for Drupal Services' node resource.
-    Drupal.Backbone.NodeModel = Drupal.Backbone.Model.extend({
-      urlRoot: "/backbone/rest/node",
-      idAttribute: "nid", 
+      // Currently just a no-op, for later use once we get to Backbone views integration.
+      Drupal.Backbone.View = Backbone.View.extend({});
 
-      // Override toJSON function to nest all attributes in a { node: ... } key
-      // to make this work with the Services module implementation of node PUSH/PUT.
-      toJSON: function() {
-        var data = {
-          node: _.clone(this.attributes)
-        };
-        return data;
-      }
-    });
-  });
+      // Node-specific settings for Drupal Services' node resource.
+      Drupal.Backbone.NodeModel = Drupal.Backbone.Model.extend({
+        urlRoot: "/backbone/rest/node",
+        idAttribute: "nid", 
+
+        // Override toJSON function to nest all attributes in a { node: ... } key
+        // to make this work with the Services module implementation of node PUSH/PUT.
+        toJSON: function() {
+          var data = {
+            node: _.clone(this.attributes)
+          };
+          return data;
+        }
+      });
+
+      // Create collection for Node resource's index interface.
+      Drupal.Backbone.NodeIndexCollection = Drupal.Backbone.Collection.extend({
+        model: Drupal.Backbone.NodeModel,
+        url: function() {
+          return this.restEndpoint + "/node.json";
+        }
+      });
+    }
+  };
 
 })(jQuery);
 
--- tests/backbone.test.js
+++ tests/backbone.test.js
@@ -12,7 +12,7 @@
     },
     test: function() {
       // Expect assertions contained in async calls.
-      expect(3);
+      expect(8);
 
       // Stop the test runner, we will restart when all of our async tests have completed.
       // TODO: Look into adding an asyncTest interface for Drupal QUnit.
@@ -22,22 +22,22 @@
       // Create a jQuery queue to make nested asyn easier. (Otherwise these
       // would need to be nested within eachother).
       // TODO: Look into $.deffered for handling this more robustly.
-      var crudQueue = $({});
+      var asyncQueue = $({});
       var newNid = null;
       
       // A factory function that uses closures to produce success and error callbacks 
       // for our Ajaj tests.
-      var dequeueFuncs = function(stageName) {
+      var dequeueFuncs = function(msg) {
         return {
           success: function() {
-            ok(true, Drupal.t('Drupal Backbone CRUD REST Call ' + stageName));
-            crudQueue.dequeue('crudQueue');
+            ok(true, Drupal.t(msg));
+            asyncQueue.dequeue('asyncQueue');
           }, 
           error: function() {
-            ok(false, Drupal.t('Drupal Backbone CRUD REST Call ' + stageName));
+            ok(false, Drupal.t(msg));
             // After erroring out, don't continue with tests.
             // TODO: determine how to clean up if we've created but not destroyed our test node.
-            crudQueue.clearQueue('crudQueue');
+            asyncQueue.clearQueue('asyncQueue');
             // Restart QUnit tests, continue on to next test.
             start();
           }
@@ -53,32 +53,54 @@
       // We use a queue to manage asynchronous processes, so each stage in the
       // CRUD runs after the prior callback has completed.
       // TODO Load that node and confirm create worked.
-      crudQueue.queue('crudQueue', function() {
-        newNode.save( {}, dequeueFuncs('Create'));
+      asyncQueue.queue('asyncQueue', function() {
+        newNode.save( {}, dequeueFuncs('Drupal CRUD REST call: Create'));
       });
 
       // Update that node.
       // TODO Load node and test the update actually worked.
-      crudQueue.queue('crudQueue', function() {
+      asyncQueue.queue('asyncQueue', function() {
         newNid = newNode.get('nid');
-        console.dir(newNid);
         newNode.set('title', 'Updated Title');
-        newNode.save( {}, dequeueFuncs('Update'));
+        newNode.save( {}, dequeueFuncs('Drupal CRUD REST call: Update'));
       });
 
+      // Test Node Index, since we have a node created.
+      var indexCollection = new Drupal.Backbone.NodeIndexCollection();
+      asyncQueue.queue('asyncQueue', function() {
+        indexCollection.fetch(_.extend(dequeueFuncs('Fetch Node Index collection'), {data: {pagesize: 1}}));
+      });
+
+      // Test returned collection.
+      // TODO test offset/page number.
+      asyncQueue.queue('asyncQueue', function() {
+        equals(indexCollection.length, 1, Drupal.t('Node Index fetch returned one node.'));
+        equals(indexCollection.at(0).get('title'), 'Updated Title', Drupal.t('Top node in Node Index is correct one.'));
+        asyncQueue.dequeue('asyncQueue'); 
+      });
+ 
+      // Test pagesize=0, should always be empty.
+      asyncQueue.queue('asyncQueue', function() {
+        indexCollection.fetch(_.extend(dequeueFuncs('Fetch Node Index collection'), {data: {pagesize: 0}}));
+      });
+      asyncQueue.queue('asyncQueue', function() {
+        equals(indexCollection.length, 0, Drupal.t('Node Index page size setting worked correctly, no nodes returned.'));
+        asyncQueue.dequeue('asyncQueue'); 
+      });
+ 
       // Delete that node.
       // TODO Attempt to load node again (should fail)
-      crudQueue.queue('crudQueue', function() {
-        newNode.destroy(dequeueFuncs('Destroy'));
+      asyncQueue.queue('asyncQueue', function() {
+        newNode.destroy(dequeueFuncs('Drupal CRUD REST call: Destroy'));
       });
 
       // Last queue item: wrap up this test.
-      crudQueue.queue('crudQueue', function() {
+      asyncQueue.queue('asyncQueue', function() {
         start();
       });
 
       // Run the queued operaitons: Go!
-      crudQueue.dequeue('crudQueue');
+      asyncQueue.dequeue('asyncQueue');
     }
   };
 
